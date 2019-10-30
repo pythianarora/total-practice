65780cf260eb283c14e7469a1531902e56a86c0f
pythianarora <arora@pythian.com>
2019-10-30 16:22:59 +0530

    Azure sample az powershell scripts

---
+++ sample-az-scripts/_connect_to_azure_account_with_creds.ps1
@@ -0,0 +1,5 @@
+﻿$key = get-content C:\client-work\Azure\securepass.key
+$creds = New-Object System.Management.Automation.PSCredential ("arora@pythian.com", ("76492d1116743f0423413b16050a5345MgB8AEsAdwBkAHAARwBsADgAeQAwADkASQBrAEEAegBlAEEAZABDADcAMwBBAFEAPQA9AHwAZgBhAGUANwBhADYAMABiADcAZABmADMAYQBjADQAYQBiAGUANABjADAANAA3AGMANwBiADcAZQAwADkAMABiADcAYQAzADYAZQBiADQAZQA1ADQAOABiADkAOQA4AGIAMwBhADkAMwA1ADMAMABjAGYAZQBhADQAMwA2ADgAZQA0AGQANQA1AGIAYQBiADcAMQA3ADEAYwAyADgAZABhADMAMAAyAGUANwAxADgANAAyAGEAZgA0ADgANQA5ADUA" | ConvertTo-SecureString -Key $key))
+$tenantid = "29a10a41-1f81-4762-87a1-4a91742c70e1"
+$subscription = "55e0c3fd-3e57-4793-bc95-9b7afe572d8b"
+Connect-AzAccount -Credential $creds -Tenant $tenantid -Subscription $subscription
---
+++ sample-az-scripts/_create_key_and_convert_password_into_secure_hash.ps1
@@ -0,0 +1,19 @@
+﻿<# -- Create a Key File to be used
+$Key = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$key | Out-File C:\client-work\Azure\securepass.key
+#>
+
+#alternate key
+#$key = (([System.DirectoryServices.ActiveDirectory.Domain]::getcurrentdomain().GetDirectoryEntry().objectSid)[0])
+
+#convert password into secure hash
+#"Y0urP@ssw0rd!" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -Key (get-content C:\client-work\Azure\securepass.key.key)
+
+#verify the username and password with the key
+$key = get-content C:\client-work\Azure\securepass.key.key
+$creds = New-Object System.Management.Automation.PSCredential("arora@pythian.com", ("76492d1116743f0423413b16050a5345MgB8AEsAdwBkAHAARwBsADgAeQAwADkASQBrAEEAegBlAEEAZABDADcAMwBBAFEAPQA9AHwAZgBhAGUANwBhADYAMABiADcAZABmADMAYQBjADQAYQBiAGUANABjADAANAA3AGMANwBiADcAZQAwADkAMABiADcAYQAzADYAZQBiADQAZQA1ADQAOABiADkAOQA4AGIAMwBhADkAMwA1ADMAMABjAGYAZQBhADQAMwA2ADgAZQA0AGQANQA1AGIAYQBiADcAMQA3ADEAYwAyADgAZABhADMAMAAyAGUANwAxADgANAAyAGEAZgA0ADgANQA5ADUA" | ConvertTo-SecureString -Key $key))
+$user = $creds.GetNetworkCredential().UserName
+$pass = $creds.GetNetworkCredential().Password
+$user
+$pass 
