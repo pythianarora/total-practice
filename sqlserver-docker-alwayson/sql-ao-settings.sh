82025aac43ec324992566d9347687c32fb19f57d
pythianarora <arora@pythian.com>
2020-04-26 21:48:27 +0530

    sql agent doesn't come online sometimes

--- sqlserver-docker-alwayson/sql-ao-settings.sh
+++ sqlserver-docker-alwayson/sql-ao-settings.sh
@@ -2,3 +2,6 @@
 sleep 90s
 # run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "P@ssw0rd" -i sql-ao-setup.sql
+# ensuring sql agent is up and restarting mssql service
+/opt/mssql/bin/mssql-conf set sqlagent.enabled true 
+systemctl restart mssql-server
