ffe435d31d0f0bd7efbab0b833cc8c9cf1dd1f46
pythianarora <arora@pythian.com>
2020-04-26 21:27:57 +0530

    Fixing the scripts

--- sqlserver-docker-alwayson/sql-ao-settings.sh
+++ sqlserver-docker-alwayson/sql-ao-settings.sh
@@ -1,4 +1,4 @@
 # wait for the SQL Server to come up
-sleep 30s
+sleep 90s
 # run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "P@ssw0rd" -i sql-ao-setup.sql
