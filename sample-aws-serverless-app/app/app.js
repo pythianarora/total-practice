8f5c35c04ed2b90b05dcd4f39c6ab0743021d323
pythianarora <arora@pythian.com>
2020-06-07 11:35:48 +0530

    AWS SA Training

---
+++ sample-aws-serverless-app/app/app.js
@@ -0,0 +1,27 @@
+const invokeUrl = 'https://u3q98crle1.execute-api.us-east-1.amazonaws.com/findfriends';
+
+// Do not edit below this line
+
+var app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      loading: true,
+      errored: false,
+      friends: null
+    }
+  },
+  mounted() {
+    axios
+      .get(invokeUrl)
+      .then(response => {
+        console.log(response)
+        this.friends = response.data.friends
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+})
