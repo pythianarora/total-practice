2a946620b1519be0d6672e4c606b654e33444d48
Sandeep Arora <pythianarora@users.noreply.github.com>
2020-06-18 20:07:08 +0530

    lb & as

--- sample-aws-lb-as/index.php
+++ sample-aws-lb-as/index.php
@@ -2,16 +2,18 @@
 <title> Best PS4 Games </title>
 <body>
 <h1>
+<center>
 <?php
 $eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
-echo '[' +$eip + '] says, these are the best PS4 games' ;
+echo "[" . $eip . "] says, these are the best PS4 games" ;
 ?>
+</center>
+</h1>
 <body>
 <br>
-<IMG SRC="God_of_War.jpg" ALT="omg"><br>
-<IMG SRC="horizon-zero-dawn.jpg" ALT="omg"><br>
-<IMG SRC="Uncharted.jpg" ALT="omg"><br>
+<IMG SRC="God_of_War.jpg" ALT="GOW" width="400" height="400">
+<IMG SRC="horizon-zero-dawn.jpg" ALT="HZD" width="400" height="400">
+<IMG SRC="Uncharted.jpg" ALT="U4" width="400" height="400">
 <body>
-</h1>
 </body>
 </html>
