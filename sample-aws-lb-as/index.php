f5e7bdb6e24306dee4c94ec35b5b245bd1236c37
Sandeep Arora <pythianarora@users.noreply.github.com>
2020-06-18 19:33:10 +0530

    autoscalinggroups

---
+++ sample-aws-lb-as/index.php
@@ -0,0 +1,17 @@
+<html>
+<title> Best PS4 Games </title>
+<body>
+<h1>
+<?php
+$eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
+echo '[' +$eip + '] says, these are the best PS4 games' ;
+?>
+<body>
+<br>
+<IMG SRC="God_of_War.jpg" ALT="omg"><br>
+<IMG SRC="horizon-zero-dawn.jpg" ALT="omg"><br>
+<IMG SRC="Uncharted.jpg" ALT="omg"><br>
+<body>
+</h1>
+</body>
+</html>
