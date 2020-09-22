b3d6d1bbc13006eb170d40a1e7356ec48d0511d3
pythianarora <arora@pythian.com>
2020-09-22 16:57:01 +0530

    Power Shell Script to Submit Deployments

--- azure-datafactory-monitoring/ADF-Alerts/ADFAlertDeploy.ps1
+++ azure-datafactory-monitoring/ADF-Alerts/ADFAlertDeploy.ps1
@@ -1,7 +1,11 @@
-$resource_group_name = "azure-datafactory-selfhosted-ir"
-$location = "North Europe"
+$resource_group_name = "<<Enter the Resource Group Name to create the deployment>>"
+$location = "<<Enter Azure Location>>"
 $deployment_time = Get-Date -Format yyyyMMddHHmmss
 $deployment_name = "adfdeployment" + "-" + $deploymentTime
-$template_file = (Get-Location).Path + "\" + "template.json"
+$template_failedpipeline_file = (Get-Location).Path + "\" + "template.failedpipeline.json"
+$template_failedtrigger_file = (Get-Location).Path + "\" + "template.failedtrigger.json"
+$template_irhighcpu_file = (Get-Location).Path + "\" + "template.ircpuhigh.json"
 $param_file = (Get-Location).Path + "\"  + "parameter.json"
-New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_file -TemplateParameterFile $param_file
+New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_failedpipeline_file -TemplateParameterFile $param_file
+New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_failedtrigger_file -TemplateParameterFile $param_file
+New-AzureRmResourceGroupDeployment -Name $deployment_name -ResourceGroupName $resource_group_name -TemplateFile $template_irhighcpu_file -TemplateParameterFile $param_file
