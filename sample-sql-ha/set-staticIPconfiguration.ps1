05ae74ccfe87075ca73972704cfab2c6e0b2ca69
arora <arora@pythian.com>
2021-04-20 14:46:33 +0530

    Create set-staticIPconfiguration.ps1

---
+++ sample-sql-ha/set-staticIPconfiguration.ps1
@@ -0,0 +1,18 @@
+ param (
+    [string]$dnsIP
+)
+Set-ExecutionPolicy Bypass -Scope Process -Force
+#Get All the adapters that are on the Server
+$adapters = gwmi -cl win32_networkadapterconfiguration | ? {($_.ipaddress) -and $_.dhcpEnabled -eq 'True' } 
+foreach ($adapter in $adapters) {
+    # Get original settings
+    $ipAddress = $adapter.IPAddress[0]
+    $subnetMask = $adapter.IPSubnet[0]
+    $dnsServers = $dnsIP
+    $defaultGateway = $adapter.DefaultIPGateway
+ 
+    # Set to static and set dns and gateway:
+    $adapter.EnableStatic($ipAddress,$subnetMask)
+    $adapter.SetDNSServerSearchOrder($dnsServers)
+    $adapter.SetGateways($defaultGateway)
+} 
